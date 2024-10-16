f3837bddba60ce872df28cb49d86f9ee29991490
Kevin Bahari <kevinrenaldibahari25@gmail.com>
2024-10-16 22:59:29 +0800

    5th Commit

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
+import state from '../store'
+import { useSnapshot } from 'valtio'
+
+
+
 const Tab = () => {
+  const snap = useSnapshot(state);
   return (
     <div>Tab</div>
   )
